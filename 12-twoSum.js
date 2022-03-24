30526aa58adb13603c7374f3889cf8a8dd693ee9
iAli22 <ali_mahde22@yahoo.com>
2022-03-24 23:40:17 +0200

    Sum of Two Integers

--- 12-twoSum.js
+++ 12-twoSum.js
@@ -1,1 +1,12 @@
-var getSum = function (a, b) {};
+var getSum = function (a, b) {
+  let temp;
+  while (b !== 0) {
+    temp = (a & b) << 1;
+    a = a ^ b;
+    b = temp;
+  }
+
+  return a;
+};
+
+console.log(getSum(1, 2));
